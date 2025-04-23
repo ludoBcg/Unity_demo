0cde05d7496ef7df9b8a5501758e1d4499e292a2
ludoBcg <blache.ludovic@gmail.com>
2025-04-23 16:17:00 +0200

    update gallery scene

---
+++ Assets/MyAssets/MyScripts/1_DoorOpener.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    private Animator doorAnimator;
+
+
+    void Start()
+    {
+        // Get the Animator component attached to the same GameObject as this script
+        doorAnimator = GetComponent<Animator>();
+        Debug.Log("Start");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object entering the trigger is the player (or another specified object)
+        if (other.CompareTag("Player")) // Make sure the player GameObject has the tag "Player"
+        {
+            if (doorAnimator != null)
+            {
+                // Trigger the Door_Open animation
+                doorAnimator.SetTrigger("Door_Open");
+            }
+        }
+    }
+}
